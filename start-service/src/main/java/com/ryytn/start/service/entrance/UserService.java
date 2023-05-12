ee2636b43c4f102776b608ca121cb9e76ab2263a
tony <xuxukang@ryytn.net>
2023-05-12 09:58:38 +0800

    1. 拆分bootstrap.yml部分配置内容至application.yml，只保留springcloud部分配置 2. 集成dubbo2.7.23

--- start-service/src/main/java/com/ryytn/start/service/entrance/UserService.java
+++ start-service/src/main/java/com/ryytn/start/service/entrance/UserService.java
@@ -1,9 +1,11 @@
 package com.ryytn.start.service.entrance;
 
 import cn.hutool.extra.spring.SpringUtil;
+import com.ryytn.start.api.service.DubboUserService;
 import com.ryytn.start.common.dto.request.UserAddOrUpdateReq;
 import com.ryytn.start.service.action.local.user.AddOrUpdateUser;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.dubbo.config.annotation.DubboService;
 import org.springframework.stereotype.Service;
 
 /**
@@ -12,9 +14,10 @@ import org.springframework.stereotype.Service;
  * @author tony
  * @date 4/7/23
  */
+@DubboService(version = "1.0.0", interfaceClass = DubboUserService.class)
 @Service
 @Slf4j
-public class UserService {
+public class UserService implements DubboUserService {
 
   public boolean addOrUpdateUser(UserAddOrUpdateReq req) {
     return SpringUtil.getBean(AddOrUpdateUser.class).invoke(req);
